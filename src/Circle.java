04fcc314482003d57da1dd6b08b9d3019a1c1090
Kamil <stacho688@gmail.com>
2017-03-01 22:56:18 +0100

    Added circle methods

--- src/Circle.java
+++ src/Circle.java
@@ -1,5 +1,26 @@
 /**
  * Created by staho on 01.03.2017.
  */
-public class Circle {
+public class Circle extends Figure{
+    private double radius;
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    @Override
+    double calculatePerimeter(){
+        return 2*Math.PI*radius;
+    }
+
+    @Override
+    double calculateArea(){
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    @Override
+    public void print(){
+        System.out.println("Circle with radius: " + radius);
+        printInfo();
+    }
 }
